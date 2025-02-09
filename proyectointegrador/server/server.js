d33a80fef5d05ebcd282cfd932a1c94568a9bde7
Ostin22 <rrcarrion@puce.edu.ec>
2025-02-09 18:34:33 -0500

    vista de administrador implementada

--- proyectointegrador/server/server.js
+++ proyectointegrador/server/server.js
@@ -32,6 +32,7 @@ dotenv.config();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 /*Rutas*/
 
